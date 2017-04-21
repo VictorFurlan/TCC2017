79a11da942141c19fd79a51be393762e00a1fa18
gabro <gabro@DESKTOP-O5CUHKB>
2017-04-21 17:16:01 -0300

    PRIMEIRO COMMIT UHUUL

---
+++ BeFreeApp/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+
+namespace BeFreeApp
+{
+    [Activity(Label = "BeFreeApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int cont = 1;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+        }
+    }
+}
+
